bf6f00616221287034de4cc6fcce9af86cdf9e38
thilagan7 <36805996+thilagan7@users.noreply.github.com>
2018-02-25 11:27:19 +0800

    Add files via upload

---
+++ MyThread1.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MyThread;
+
+
+ 
+class MyThread extends Thread {
+
+    public void run() {
+        System.out.println("running...");
+    }
+
+    public static void main(String args[]) {
+        MyThread t1 = new MyThread();
+        MyThread t2 = new MyThread();
+        System.out.println("Name of t1:" + t1.getName());
+        System.out.println("Name of t2:" + t2.getName());
+        System.out.println("id of t1:" + t1.getId());
+
+        t1.start();
+        t2.start();
+
+        t1.setName("STIW3054");
+        System.out.println("After changing name of t1:" + t1.getName());
+    }
+}
